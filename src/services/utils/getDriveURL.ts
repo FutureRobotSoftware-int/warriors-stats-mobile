d32459510c016c2d1f85ade20a88e8218b74c498
nicolas-frs <nicolas@futurerobot.dev>
2025-07-09 20:48:07 -0500

    Update getDriveURL.ts

--- src/services/utils/getDriveURL.ts
+++ src/services/utils/getDriveURL.ts
@@ -1,3 +1,4 @@
 export function getGoogleDriveVideoUrl(id: string) {
-    return `https://drive.google.com/uc?export=download&id=${id}`
+    // return `https://drive.google.com/uc?export=download&id=${id}`
+    return `https://www.googleapis.com/drive/v3/files/${id}?alt=media&key=AIzaSyBKJF5do5iaLEranDvxHSHUOj-sAUWYQWw`
 }
